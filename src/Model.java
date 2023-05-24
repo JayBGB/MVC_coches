30f9371b9d1ec57ddf5c2eae719726af7918da01
Borja G.B <bgarciabarrera@danielcastelao.org>
2023-05-24 13:48:21 +0200

    Modifico Dialog.form para que se pueda estirar la ventana

--- src/Model.java
+++ src/Model.java
@@ -5,11 +5,12 @@ public class Model {
 
     /**
      * Crea un coche y lo mete en el parking
-     * @param modelo del coche
+     *
+     * @param modelo    del coche
      * @param matricula identificador unico
      * @return el coche creado
      */
-    public Coche crearCoche(String modelo, String matricula){
+    public Coche crearCoche(String modelo, String matricula) {
         Coche aux = new Coche(modelo, matricula);
         parking.add(aux);
         return aux;
@@ -17,7 +18,7 @@ public class Model {
 
     /**
      * @param matricula
-     * @param v nueva velocidad
+     * @param v         nueva velocidad
      * @return velocidad modificada
      */
     public Integer cambiarVelocidad(String matricula, Integer v) {
@@ -29,13 +30,14 @@ public class Model {
 
     /**
      * Busca coche segun matricula
+     *
      * @param matricula a buscar
-     * @return chche o null si no existe
+     * @return coche o null si no existe
      */
-    public Coche getCoche(String matricula){
+    public Coche getCoche(String matricula) {
         Coche aux = null;
         // recorre el array buscando por matricula
-        for (Coche e: parking) {
+        for (Coche e : parking) {
             if (e.matricula.equals(matricula)) {
                 aux = e;
             }
@@ -66,11 +68,13 @@ public class Model {
     }
 
     /**
-     * Ddevuelve la velocidad segun la matricula
+     * Devuelve la velocidad segun la matricula
+     *
      * @param matricula
      * @return
      */
     public Integer getVelocidad(String matricula) {
         return getCoche(matricula).velocidad;
     }
+
 }
