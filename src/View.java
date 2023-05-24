e9c6e07a3f90eae7957dbb54f6726e14ad2f8d8a
Borja G.B <bgarciabarrera@danielcastelao.org>
2023-05-24 13:46:29 +0200

    Añado método muestraDatosCoche a View

--- src/View.java
+++ src/View.java
@@ -2,4 +2,8 @@ public class View {
     void muestraVelocidad(String matricula, Integer v){
         Dialog.vDialogo("El coche " + matricula + " tiene una velocidad de: " + v + "km/h");
     }
+
+    public void muestraDatosCoche(String modelo, String matricula, Integer velocidad) {
+        Dialog.vDialogo("El coche con matrícula " + matricula + " es un " + modelo + " y actualmente tiene una velocidad de: " + velocidad + "km/h");
+    }
 }
