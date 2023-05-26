d91dd849b18230c8b80bb80974f8429c2845179f
Borja G.B <bgarciabarrera@danielcastelao.org>
2023-05-26 09:11:13 +0200

    Corregido dialogo

--- src/View.java
+++ src/View.java
@@ -1,5 +1,10 @@
 public class View {
+    /**
+     * Muestra la velocidad del coche
+     * @param matricula
+     * @param v
+     */
     void muestraVelocidad(String matricula, Integer v){
-        Dialog.vDialogo("El coche " + matricula + " tiene una velocidad de: " + v + "km/h");
+        Dialog.vDialogo("El coche con matrícula" + matricula + " va a una velocidad de: " + v + "km/h");
     }
 }
